7219ef460cc86589fc9840fb6fe22eec24797ec5
liusheng <liusheng@taotaosou.com>
2015-07-15 11:23:24 +0800

    develop gulp

---
+++ app/scripts/test.js
@@ -0,0 +1,7 @@
+/**
+ * Created by liusheng on 7/15/15.
+ */
+
+import test1 from './test/test1.js';
+
+console.log(test1.string);
---
+++ app/scripts/test/test1.js
@@ -0,0 +1,7 @@
+/**
+ * Created by liusheng on 7/15/15.
+ */
+
+export default {
+	string: 'string'
+}
